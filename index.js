49ad7b6df12a293ed624308447e3016804cc2d97
ksh009 <khughessean01@gmail.com>
2024-06-08 13:30:30 +0200

    endpoint single country

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ const app = express();
 app.use(cors());
 const port = 5000;
 
-app.get("/flags", (req, res) => {
+app.get("/countries", (req, res) => {
   try {
     res.status(200).json(flagData);
   } catch (error) {
     res.status(500).json(error);
   }
 });
+
+app.get("/country/:name", (req, res) => {
+    try {
+      const countryName = req.params.name;
+      const country = flagData.find((country) => country.common_name === countryName);
+      if (country) {
+        res.status(200).json(country);
+      } else {
+        res.status(404).json({ message: "Country not found" });
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
 
 app.listen(port, () => {
   console.log(`Flag service is running at http://localhost:${port}`);
