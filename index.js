8e69dc56e7d2f5f9457f18732e71f38567d526f7
ksh009 <khughessean01@gmail.com>
2024-06-11 14:51:40 +0200

    update filename

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.use(express.json({ extended: false }));
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "https://restful-countries-ksh.netlify.app",
+  // origin: "https://restful-countries-ksh.netlify.app",
+  origin: "http://localhost:4200",
 };
 
 app.use(cors(corsOptions));
