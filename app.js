ac084c664ae81b6010d37314d235930c28423312
ksh009 <khughessean01@gmail.com>
2024-06-11 12:39:49 +0200

    cleanup

--- app.js
+++ app.js
@@ -13,32 +13,8 @@ const port = 5000;
 app.use(cors());
 app.use(routes);
 
-// app.get("/api/countries", (req, res) => {
-//   try {
-//     res.status(200).json(flagData);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
-
-// app.get("/api/country/:name", (req, res) => {
-//   try {
-//     const countryName = req.params.name;
-//     const country = flagData.find(
-//       (country) => country.common_name === countryName
-//     );
-//     if (country) {
-//       res.status(200).json(country);
-//     } else {
-//       res.status(404).json({ message: "Country not found" });
-//     }
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
-
 app.listen(port, () => {
   console.log(`Flag service is running at http://localhost:${port}`);
 });
 
-// module.exports = app;
+module.exports = app;
